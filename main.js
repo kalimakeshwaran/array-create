85d06f8889f0f96f5fee5ea0dbd9927b473785c3
kalimakeshwaran <makeshvijay735@gmail.com>
2022-12-20 16:40:48 +0530

    add

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+//update and delete method array //
+var a=parseInt(prompt("enter the value"))
+var b=[a];
+for(i=0;i<a;i++){
+    b[i]=parseInt(prompt("enter"))
+}
+document.write(b+"<br>")
+
+
+var c=parseInt(prompt("enter the c value"))
+b[c]=parseInt(prompt("enter the num"))
+document.write(b)     
